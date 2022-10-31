abf843482e422eff52e81458a175f0354f65281d
Prashant Singh <octanesingh@gmail.com>
2022-10-31 20:06:38 +0530

    Adding the user reducer

--- store/slices/userReducer.js
+++ store/slices/userReducer.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
-    value: "Just checking",
+    value: null,
   },
   reducers: {
     login: (state, action) => {
